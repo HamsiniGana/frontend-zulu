40df86aef450e2c854532ffadcd8c371e023c440
Hamsini Ganesan <hamsinig123@gmail.com>
2026-02-26 17:24:20 +1100

    first commit - adding homepage

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import plant from './assets/plant.png'
+
+function App() {
+  return (
+    <div className="bg-medium-green min-h-screen flex">
+      <div className="bg-light-green w-full m-[50px] rounded-3xl flex flex-col items-center ">
+        <div className="flex flex-row justify-end my-5 mr-[50px] mb-[160px] gap-5 w-full ">
+          <button class="bg-medium-green hover:bg-black hover:text-white text-black text-xl font-bold py-5 px-[40px] rounded-full">
+            Login
+          </button>
+          <button class="bg-dark-green hover:bg-black text-white font-bold text-xl py-5 px-[40px] rounded-full">
+            Sign up
+          </button>
+        </div>
+        <h1 className='text-[60px]'>
+          Welcome to Zulu!
+          </h1>
+          <p className='text-[30px]'>
+            Aligning your garden’s needs with the day’s forecast
+          </p>
+          <img src={plant} className='size-60 mt-[80px] mt-[70px]'/>
+      </div>
+    </div>
+  );
+}
+export default App;
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+   content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      'dark-green': '#2A603E',
+      'medium-green':'#B9DCA2',
+      'light-green': '#E9F8E2',
+      'white': '#ffffff',
+      'black': '#000000'
+    }
+  },
+  plugins: [],
+}
+
