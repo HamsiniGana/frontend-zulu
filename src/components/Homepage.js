4caa56ba1f0627a315db036c826ce5f9daeeb055
Hamsini Ganesan <hamsinig123@gmail.com>
2026-02-28 15:45:59 +1100

    changing layouts of elements inside the tiles

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -13,18 +13,18 @@ export default function Homepage () {
                         rounded-3xl shadow-2xl shadow-dark-bottle-green
                         hover:scale-[1.01] h-[40vh] mt-[5vh]">
           <div className='flex flex-col gap-2 h-full items-center justify-center mb-2'>
-            <img src={rain} alt='rain-icon' className='w-[50px] h-[50px]'/>
-            <img src={sappling} alt='sappling-icon' className='w-[40px] h-[40px]'/>
+            <img src={rain} alt='rain-icon' className='w-[80px] h-[80px]'/>
+            <img src={sappling} alt='sappling-icon' className='w-[70px] h-[70px]'/>
           </div>
 
           <div className="flex flex-col flex-1 h-full">
             <div className="flex flex-col flex-1 justify-center">
               <h1 className="text-dark-bottle-green font-bold text-2xl pb-5">Data</h1>
-              <p className="text-lg">Upload or use existing datasets to analyse plant data in relation to weather data.</p>
+              <p className="text-xl">Upload or use existing datasets to analyse plant data in relation to weather data.</p>
             </div>
             <div className="flex flex-row justify-end mr-1">
                 <button  className="hover:bg-white hover:text-black py-2 px-3 rounded-xl w-[150px] hover:border hover:border-solid hover:border-black
-                bg-dark-bottle-green text-white">Get started</button>
+                bg-dark-bottle-green text-white text-xl">Get started</button>
             </div>
           </div>
         </div>
@@ -38,30 +38,32 @@ export default function Homepage () {
             <h1 className="text-dark-bottle-green font-bold text-2xl pb-5">Graphs</h1>
           </div>
 
-          <div className='flex flex-row gap-5 justify-start flex-1'>
-            <img src={graph} alt='graph-icon' className='w-[50px] h-[45px]'/>
-            <p className="text-lg pb-5">View data trends.</p>
+          <div className='flex flex-row gap-5 items-center flex-1 '>
+            {/* <div className='flex flex-row items-center h-full  border border-solid border-black'>   */}
+              <img src={graph} alt='graph-icon' className='w-[80px] h-[80px] '/>
+              <p className="text-xl pb-5 text-center mt-5">View data trends.</p>
+            {/* </div> */}
           </div>
           <div className="flex flex-row justify-end mr-1">
               <button  className="hover:bg-white hover:text-black py-2 px-3 rounded-xl w-[150px] hover:border hover:border-solid hover:border-black
-              bg-dark-bottle-green text-white">Get started</button>
+              bg-dark-bottle-green text-white text-xl">Get started</button>
             </div>
         </div>
-        <div className='flex flex-col bg-gradient-to-r from-medium-green 
+        <div className='flex flex-col bg-gradient-to-r from-medium-green
                         via-light-green to-medium-green m-5 p-5
                         rounded-3xl shadow-2xl shadow-dark-bottle-green
-                        hover:scale-[1.01] w-[45vw]  h-[35vh]'>
+                        hover:scale-[1.01] w-[49vw]  h-[40vh]'>
           <div className="flex flex-row justify-start w-full">
             <h1 className="text-dark-bottle-green font-bold text-2xl pb-5">Reports</h1>
           </div>
 
-          <div className='flex flex-row gap-5 justify-start flex-1'>
-            <img src={report} alt='graph-icon' className='w-[50px] h-[50px]'/>
-            <p className="text-lg pb-5">Harvest final intelligence.</p>
+          <div className='flex flex-row gap-5 items-center flex-1'>
+            <img src={report} alt='graph-icon' className='w-[80px] h-[80px]'/>
+            <p className="text-xl pb-5 mt-5">Harvest final intelligence.</p>
           </div>
           <div className="flex flex-row justify-end mr-1">
               <button  className="hover:bg-white hover:text-black py-2 px-3 rounded-xl w-[150px] hover:border hover:border-solid hover:border-black
-              bg-dark-bottle-green text-white">Get started</button>
+              bg-dark-bottle-green text-white text-xl">Get started</button>
             </div>
         </div>
         </div>
