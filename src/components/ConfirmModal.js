bfc1805d13c9d51114b8c7eed9ef662bfc654b5d
Hamsini Ganesan <hamsinig123@gmail.com>
2026-03-17 15:42:28 +1100

    connected login and logout endpoints

--- src/components/ConfirmModal.js
+++ src/components/ConfirmModal.js
@@ -1,44 +1,73 @@
-import { useEffect, useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
-import axios from 'axios';
+import { useEffect, useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export default function ConfirmModal(props) {
-    const [show, setShow] = useState(true);
+  const nav = useNavigate();
+  const [show, setShow] = useState(true);
 
-    const handleClose = () => setShow(false);
-    // const handleShow = () => setShow(true);
+  const handleClose = () => setShow(false);
+  // const handleShow = () => setShow(true);
 
-    const logout_fn = async() => {
-        // fetch usernmae from locaStorage to logout
-        // when user logs in store username in localStorage
+  const logout_fn = async () => {
+    const username = localStorage.getItem("username");
+    localStorage.removeItem("username");
+    console.log(username);
+
+    const res = await axios({
+      method: "post",
+      url: "https://sengzulu.gentlehill-6b9262ed.australiaeast.azurecontainerapps.io/logout/",
+      params: {
+        username: username,
+      },
+    });
+
+    if (res.status === 200) {
+      nav("/login");
     }
+  };
 
-    useEffect(() => {
-        setShow(props.showLogoutModal)
-    }, [props.showLogoutModal])
+  useEffect(() => {
+    setShow(props.showLogoutModal);
+  }, [props.showLogoutModal]);
 
-    return (
+  return (
     <>
       <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton onClick={() => props.setShowLogoutModal(false)}>
+        <Modal.Header
+          closeButton
+          onClick={() => props.setShowLogoutModal(false)}
+        >
           <Modal.Title>Logout request</Modal.Title>
         </Modal.Header>
-        <Modal.Body>
-          Are you sure you want to logout?
-        </Modal.Body>
+        <Modal.Body>Are you sure you want to logout?</Modal.Body>
         <Modal.Footer>
-          <Button onClick={() => props.setShowLogoutModal(false)}
-          style={{backgroundColor: "var(--medium-green)", borderColor: "var(--medium-green)"}}
-          className='text-black'>
+          <Button
+            onClick={() => props.setShowLogoutModal(false)}
+            style={{
+              backgroundColor: "var(--medium-green)",
+              borderColor: "var(--medium-green)",
+            }}
+            className="text-black"
+          >
             Cancel
           </Button>
-          <Button style={{backgroundColor: "var(--dark-green)", borderColor: "var(--dark-green)"}}
-          onClick={()=>props.setShowLogoutModal(false)}>
+          <Button
+            style={{
+              backgroundColor: "var(--dark-green)",
+              borderColor: "var(--dark-green)",
+            }}
+            onClick={() => {
+              props.setShowLogoutModal(false);
+              logout_fn();
+            }}
+          >
             Yes
           </Button>
         </Modal.Footer>
       </Modal>
     </>
-    )
+  );
 }
--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,120 +1,167 @@
-import plant from '../assets/plant.png'
-import leftArrow from '../assets/arrow-left.svg'
-import { useNavigate } from 'react-router-dom'
-import axios from 'axios'
-import DisplayModal from './DisplayModal'
-import { useState } from 'react'
+import plant from "../assets/plant.png";
+import leftArrow from "../assets/arrow-left.svg";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import DisplayModal from "./DisplayModal";
+import { useState, useEffect } from "react";
 
+export default function LoginPage() {
+  const nav = useNavigate();
+  const [modalMsg, setModalMsg] = useState("");
+  const [modalTitle, setModalTitle] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-export default function LoginPage () {
-    const nav = useNavigate()
-    const [modalMsg, setModalMsg] = useState('')
-    const [modalTitle, setModalTitle] = useState('')
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
-
-    const login_fn = async() => {
-        let res = ''
-        const params = new URLSearchParams({ username: username, password: password});
-
-        try {
-            res = await axios({
-            method: 'post',
-            url: 'https://sengzulu.gentlehill-6b9262ed.australiaeast.azurecontainerapps.io/login/',
-            data: params,
-            headers: {
-                "Content-Type" : "application/x-www-form-urlencoded"
-            }
-        });
-
-        if (res.status === 200) {
-            nav('/loadingPage')
-        } else {
-            setModalMsg('Something went wrong. Please try again ☹️')
-            setModalTitle("⚠ Woops")
-        }
-
-        } catch (e) {
-            if (e.response.data.detail[0].msg !== undefined) {
-                setModalMsg(e.response.data.detail[0].msg)
-                setModalTitle("⚠ Woops")
-            } else if (e.response.data.detail !== undefined) {
-                setModalMsg(e.response.data.detail)
-                setModalTitle("⚠ Woops")
-            }
-        }
+  useEffect(() => {
+    if (username === "") return;
+    else {
+      localStorage.setItem("username", username);
     }
+  }, [username]);
 
-    return (
-        <div className="flex flex-row w-screen">
-            <div className="flex flex-col bg-light-green items-center justify-center w-1/3">
-                <div className="flex flex-row justify-start items-start w-full ">
-                    <img src={leftArrow} alt="arrow-icon" className='m-4 size-10' onClick={() => nav('/')}/>
-                </div>
-                <div className="flex flex-col flex-1 justify-center p-5 mb-5">
-                    <img src={plant} alt="plant-icon" className='size-60 mb-[70px]'/>
-                </div>
+  const login_fn = async () => {
+    let res = "";
+    const params = new URLSearchParams({
+      username: username,
+      password: password,
+    });
 
+    try {
+      res = await axios({
+        method: "post",
+        url: "https://sengzulu.gentlehill-6b9262ed.australiaeast.azurecontainerapps.io/login/",
+        data: params,
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      });
+
+      if (res.status === 200) {
+        nav("/loadingPage");
+      } else {
+        setModalMsg("Something went wrong. Please try again ☹️");
+        setModalTitle("⚠ Woops");
+      }
+    } catch (e) {
+      if (e.response.data.detail.msg !== undefined) {
+        setModalMsg(e.response.data.detail[0].msg);
+        setModalTitle("⚠ Woops");
+      } else if (e.response.data.detail !== undefined) {
+        setModalMsg(e.response.data.detail);
+        setModalTitle("⚠ Woops");
+      }
+    }
+  };
+
+  return (
+    <div className="flex flex-row w-screen">
+      <div className="flex flex-col bg-light-green items-center justify-center w-1/3">
+        <div className="flex flex-row justify-start items-start w-full ">
+          <img
+            src={leftArrow}
+            alt="arrow-icon"
+            className="m-4 size-10"
+            onClick={() => nav("/")}
+          />
+        </div>
+        <div className="flex flex-col flex-1 justify-center p-5 mb-5">
+          <img src={plant} alt="plant-icon" className="size-60 mb-[70px]" />
+        </div>
+      </div>
+      <div className="bg-medium-green min-h-screen w-2/3">
+        <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+          <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+              <h2 className="mt-10 text-center text-3xl font-bold tracking-tight text-gray-900">
+                Login to your account
+              </h2>
             </div>
-            <div className="bg-medium-green min-h-screen w-2/3">
-                <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-                    <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-                        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                            <h2 className="mt-10 text-center text-3xl font-bold tracking-tight text-gray-900">Login to your account</h2>
-                        </div>
 
-                        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                            <form onSubmit={(e) => {
-                                e.preventDefault()
-
-                            }} className="space-y-6">
-                            <div>
-                                <label htmlFor="username" className="block text-lg font-medium text-gray-900">Username:</label>
-                                <div className="mt-2">
-                                <input id="username" 
-                                type="text" name="username" 
-                                required autoComplete="username" 
-                                onChange={(e) => setUsername(e.target.value)}
-                                className="block w-full rounded-md bg-white px-3 py-1.5 text-base 
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+              <form
+                onSubmit={(e) => {
+                  e.preventDefault();
+                }}
+                className="space-y-6"
+              >
+                <div>
+                  <label
+                    htmlFor="username"
+                    className="block text-lg font-medium text-gray-900"
+                  >
+                    Username:
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      id="username"
+                      type="text"
+                      name="username"
+                      required
+                      autoComplete="username"
+                      onChange={(e) => setUsername(e.target.value)}
+                      className="block w-full rounded-md bg-white px-3 py-1.5 text-base 
                                 text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 
-                                focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
-                                </div>
-                            </div>
+                                focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                    />
+                  </div>
+                </div>
 
-                            <div>
-                                <div className="flex items-center justify-between">
-                                <label htmlFor="password" className="block text-lg font-medium text-gray-900">Password:</label>
-                                </div>
-                                <div className="mt-2">
-                                <input id="password" type="password" name="password" required autoComplete="current-password" 
-                                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1
+                <div>
+                  <div className="flex items-center justify-between">
+                    <label
+                      htmlFor="password"
+                      className="block text-lg font-medium text-gray-900"
+                    >
+                      Password:
+                    </label>
+                  </div>
+                  <div className="mt-2">
+                    <input
+                      id="password"
+                      type="password"
+                      name="password"
+                      required
+                      autoComplete="current-password"
+                      className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1
                                 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600
-                                sm:text-sm/6" 
-                                onChange={(e) => setPassword(e.target.value)}/>
-                                </div>
-                            </div>
+                                sm:text-sm/6"
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                  </div>
+                </div>
 
-                            <div>
-                                <button type="submit" className="flex w-full justify-center rounded-md bg-dark-green px-3 py-1.5 text-sm/6
+                <div>
+                  <button
+                    type="submit"
+                    className="flex w-full justify-center rounded-md bg-dark-green px-3 py-1.5 text-sm/6
                                         font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 
                                         focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                                        onClick={() => login_fn()}>Login</button>
-                            </div>
-                            </form>
-
-                        <p className="mt-10 text-center text-md text-gray-500">
-                        Don't have an account?
-                        <a href="/sign-up" className="font-semibold text-dark-green hover:underline hover: decoration-2">Sign up</a>
-                        </p>
-                    </div>
+                    onClick={() => login_fn()}
+                  >
+                    Login
+                  </button>
                 </div>
+              </form>
+
+              <p className="mt-10 text-center text-md text-gray-500">
+                Don't have an account?
+                <a
+                  href="/sign-up"
+                  className="font-semibold text-dark-green hover:underline hover: decoration-2"
+                >
+                  Sign up
+                </a>
+              </p>
             </div>
+          </div>
         </div>
-        <DisplayModal modalMsg={modalMsg}
-                      show = {modalMsg !== ''}
-                      setModalMsg={setModalMsg}
-                      modalTitle={modalTitle}
-                             />
+      </div>
+      <DisplayModal
+        modalMsg={modalMsg}
+        show={modalMsg !== ""}
+        setModalMsg={setModalMsg}
+        modalTitle={modalTitle}
+      />
     </div>
-)
+  );
 }
